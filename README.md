a00780e1a1980348611031f21c51956897c9298e
ericatsu <ekaatsu@st.ug.edu.gh>
2023-09-06 21:26:54 +0000

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# auctor_only
+
+A new Flutter project.
+
+- [App UI](https://www.uistore.design/items/open-fashion-free-ecommerce-ui-kit/)
---
+++ android/app/src/main/kotlin/com/lxidus/auctor_only/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.lxidus.auctor_only
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
